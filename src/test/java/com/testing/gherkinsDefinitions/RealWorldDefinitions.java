4cdeeae813887f0ec60e68395611fa2f544c491b
AleksGeste <a.cikinovs@icloud.com>
2020-08-03 15:22:47 +0300

    Done

--- src/test/java/com/testing/gherkinsDefinitions/RealWorldDefinitions.java
+++ src/test/java/com/testing/gherkinsDefinitions/RealWorldDefinitions.java
@@ -6,6 +6,8 @@ import cucumber.api.java.en.When;
 
 import java.io.IOException;
 
+import static net.serenitybdd.core.Serenity.sessionVariableCalled;
+
 public class RealWorldDefinitions {
   @When("^the user creates an account with following data:$")
   public void theUserCreatesAnAccountWithFollowingData(DataTable dataTable) throws IOException {
@@ -15,5 +17,37 @@ public class RealWorldDefinitions {
   @When("^the user logs into account with following data:$")
   public void theUserLogsIntoAccountWithFollowingData(DataTable dataTable) throws IOException {
     RealworldSteps.logIntoAccount(dataTable);
+  }
+
+  @When("^the user updates the settings using the following data:$")
+  public void theUserUpdatesTheSettingsUsingTheFollowingData(DataTable dataTable) throws IOException {
+    RealworldSteps.updateAccount(dataTable);
+  }
+
+  @When("^the user creates a post using the following data:$")
+  public void theUserCreatesAPostUsingTheFollowingData(DataTable dataTable) throws IOException {
+    RealworldSteps.createPost(dataTable);
+  }
+
+  @When("^the user deletes article$")
+  public void theUserDeletesArticle() {
+  RealworldSteps.deleteArticle();
+  }
+
+  @When("^the user modifies the post using the following data:$")
+  public void theUserModifiesThePostUsingTheFollowingData(DataTable dataTable) throws IOException {
+    RealworldSteps.modifies(dataTable);
+
+
+  }
+
+  @When("^the user adds a comment to the post with the following data:$")
+  public void theUserAddsACommentToThePostWithTheFollowingData(DataTable dataTable) throws IOException {
+    RealworldSteps.createComment(dataTable);
+  }
+
+  @When("^the user deletes the comment$")
+  public void theUserDeletesTheComment() {
+    RealworldSteps.deleteComment();
   }
 }
--- src/test/java/com/testing/serenitySteps/RealworldSteps.java
+++ src/test/java/com/testing/serenitySteps/RealworldSteps.java
@@ -14,55 +14,92 @@ import static net.serenitybdd.core.Serenity.sessionVariableCalled;
 import static net.serenitybdd.core.Serenity.setSessionVariable;
 
 public class RealworldSteps extends BaseSteps {
-  private final static String _API_USERS_ = "/api/users/";
-  private final static String _API_ARTICLES_ = "/api/articles/";
-  // Find endpoint for API login
-  private final static String _API_USERS_LOGIN_ = null;
+    private final static String _API_USERS_ = "/api/users/";
+    private final static String _API_USER_ = "/api/user/";
 
-  @Steps
-  RealworldSteps realworldSteps;
+    private final static String _API_ARTICLES_ = "/api/articles/";
+    // Find endpoint for API login
+    private final static String _API_USERS_LOGIN_ = null;
 
-  @Step
-  public static void createAnAccount(DataTable dataTable) throws IOException {
-    Map<String, Object> map = new HashMap<>(dataTable.asMap(String.class, String.class));
+    @Steps
+    RealworldSteps realworldSteps;
 
-    if(map.get("user --> email").toString().equals(RANDOM_EMAIL)){
-      String randomEmail = "test_" + new Random().nextInt(999999) + "@testdevlab.com";
-      map.replace("user --> email", randomEmail);
-      setSessionVariable(RANDOM_EMAIL).to(randomEmail);
+    @Step
+    public static void createAnAccount(DataTable dataTable) throws IOException {
+        Map<String, Object> map = new HashMap<>(dataTable.asMap(String.class, String.class));
+
+        if (map.get("user --> email").toString().equals(RANDOM_EMAIL)) {
+            String randomEmail = "test_" + new Random().nextInt(999999) + "@testdevlab.com";
+            map.replace("user --> email", randomEmail);
+            setSessionVariable(RANDOM_EMAIL).to(randomEmail);
+        }
+
+        if (map.get("user --> username").toString().equals(RANDOM_USERNAME)) {
+            String randomUsername = "test_" + new Random().nextInt(999999);
+            map.replace("user --> username", randomUsername);
+            setSessionVariable(RANDOM_USERNAME).to(randomUsername);
+        }
+
+        sendRequestWithBodyJson(POST, _API_USERS_, createBody(map));
+        if (((Response) sessionVariableCalled(RESPONSE)).statusCode() == 200) {
+            saveValueInPathToSessionVariable("user --> token", "token");
+        }
     }
 
-    if(map.get("user --> username").toString().equals(RANDOM_USERNAME)){
-      String randomUsername = "test_" + new Random().nextInt(999999);
-      map.replace("user --> username", randomUsername);
-      setSessionVariable(RANDOM_USERNAME).to(randomUsername);
+    @Step
+    public static void logIntoAccount(DataTable dataTable) throws IOException {
+        Map<String, Object> map = new HashMap<>(dataTable.asMap(String.class, String.class));
+        setSessionVariable("token").to(null);
+
+        if (map.get("user --> email").toString().equals(RANDOM_EMAIL)) {
+            map.replace("user --> email", sessionVariableCalled(RANDOM_EMAIL));
+        }
+
+        sendRequestWithBodyJson(POST, _API_USERS_LOGIN_, createBody(map));
+        if (((Response) sessionVariableCalled(RESPONSE)).statusCode() == 200) {
+            saveValueInPathToSessionVariable("user --> token", "token");
+        }
     }
 
-    sendRequestWithBodyJson(POST, _API_USERS_, createBody(map));
-    if (((Response) sessionVariableCalled(RESPONSE)).statusCode() == 200) {
-      saveValueInPathToSessionVariable("user --> token", "token");
-    }
-  }
 
-  @Step
-  public static void logIntoAccount(DataTable dataTable) throws IOException {
-    Map<String, Object> map = new HashMap<>(dataTable.asMap(String.class, String.class));
-    setSessionVariable("token").to(null);
+    // Private
 
-    if(map.get("user --> email").toString().equals(RANDOM_EMAIL)){
-      map.replace("user --> email", sessionVariableCalled(RANDOM_EMAIL));
+    private static String addCommentEndpoint(String slug) {
+        return _API_ARTICLES_ + slug + "/comments/";
     }
 
-    sendRequestWithBodyJson(POST, _API_USERS_LOGIN_, createBody(map));
-    if (((Response) sessionVariableCalled(RESPONSE)).statusCode() == 200) {
-      saveValueInPathToSessionVariable("user --> token", "token");
+    @Step
+    public static void updateAccount(DataTable dataTable) throws IOException {
+        sendRequestWithBodyJson(PUT, _API_USER_, createBody(dataTable));
+
+
     }
-  }
 
+    @Step
+    public static void createPost(DataTable dataTable) throws IOException {
+        sendRequestWithBodyJson(POST, _API_ARTICLES_, createBody(dataTable));
 
-  // Private
+    }
 
-  private static String addCommentEndpoint(String slug){
-    return _API_ARTICLES_ + slug + "/comments";
-  }
+    @Step
+    public static void deleteArticle() {
+        sendRequest(DELETE, _API_ARTICLES_ + sessionVariableCalled("slug"));
+
+    }
+
+    @Step
+    public static void modifies(DataTable dataTable) throws IOException {
+        sendRequestWithBodyJson(PUT, _API_ARTICLES_ + sessionVariableCalled("slug"), createBody(dataTable));
+    }
+
+    @Step
+    public static void createComment(DataTable dataTable) throws IOException {
+        sendRequestWithBodyJson(PUT, _API_ARTICLES_ + sessionVariableCalled("slug") + "/comments", createBody(dataTable));
+
+    }
+
+    @Step
+    public static void deleteComment() {
+        sendRequest(DELETE, addCommentEndpoint("slug") + sessionVariableCalled("comment_id"));
+    }
 }
